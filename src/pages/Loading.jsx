63c44cc3e34cd4617b9c5fe44f4e7123928091ed
yasmin595 <yasminsuborna8@gmail.com>
2025-05-21 11:22:15 +0600

    added loading spinner

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="min-h-screen flex  justify-center items-center">
+      <span className="loading loading-bars loading-xl"></span>
+      <span className="loading loading-bars loading-xl"></span>
+      <span className="loading loading-bars loading-xl"></span>
+      <span className="loading loading-bars loading-xl"></span>
+      <span className="loading loading-bars loading-xl"></span>
+    </div>
+  );
+};
+
+export default Loading;
