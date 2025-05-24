62f95ef4cea8d70891b43b4ad2dccf8ea14fdc24
yasmin595 <yasminsuborna8@gmail.com>
2025-05-24 16:26:16 +0600

    added my dolution

--- src/components/PostedTask.jsx
+++ src/components/PostedTask.jsx
@@ -10,7 +10,7 @@ const PostedTask = () => {
       <div className='w-11/12 mx-auto my-8  bg-gray-100 dark:bg-gray-800 p-4 '>
            <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 '>
            {
-emailTask.map((task) => {<PostedCard key={task._id} task={task}  emailTask={emailTask}  setEmailTask={setEmailTask}></PostedCard>})
+emailTask.map(task => <PostedCard key={task._id} task={task}  emailTask={emailTask}  setEmailTask={setEmailTask}></PostedCard>)
 
            }
         </div>
